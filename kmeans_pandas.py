fef1e666955938e16c2fedd0e1bda24fde53e587
Júlia Costa C. de Oliveira <50522321+juliacoliveira@users.noreply.github.com>
2021-03-21 16:47:13 -0300

    Removing some unnecessary things and commenting

--- kmeans_pandas.py
+++ kmeans_pandas.py
@@ -2,13 +2,14 @@ from scipy.stats import mode
 import pandas as pd
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
+import seaborn as sb
 import sklearn as sk
 from scipy.stats import zscore
 import numpy as np
 from sklearn.cluster import KMeans
 
 # Opening the file
-csv_file = '/diamonds_training.csv' #need to change the path of the file csv
+csv_file = 'C:/Users/julia/Downloads/7 semestre/IA/Projeto/KMEANS/diamonds_training.csv' #need to change the path of the file csv
 df = pd.read_csv(csv_file, delimiter=';')
 print (df.head(3))
 
@@ -49,55 +50,69 @@ attr_df = df.drop(['price'], axis = 1).values.tolist()
 
 targets_df = df.drop(['carat', 'color', 'table', 'x', 'y', 'z'], axis = 1).values.tolist()
 
-#Treinamento dos agrupadores K-Means usando os atributos do conjunto de treino
+#################################### Test data acquisition ######################################################
 
-def trainKMeans(m):
-  clf = []
-  for n in range(1,m+1):
-    clf.append(KMeans(n_clusters=n, n_init=10))
-    clf[n-1].fit(attr_df)
-  return clf.copy()
+# Opening the file
+csv_file = 'C:/Users/julia/Downloads/7 semestre/IA/Projeto/KMEANS/diamonds_test.csv' #need to change the path of the file csv
+df = pd.read_csv(csv_file, delimiter=';')
+print (df.head(3))
+
+indexNames = df[ df['price'] == 4].index
+df = df.drop(indexNames)
+indexNames = df[ df['price'] == 3].index
+df = df.drop(indexNames)
+
+# Getting the columns names
+linear_vars = df.select_dtypes(include = [np.number]).columns
+linear_vars =  list(linear_vars)
+
+df = removeoutliers(df, colorless_vars, 2)
+convertToLog(df, colorless_vars)
+
+attr_test = df.drop(['price'], axis = 1).values.tolist()
+
+targets_test = df.drop(['carat', 'color', 'table', 'x', 'y', 'z'], axis = 1).values.tolist()
+
+################################ End of the test acquisition #######################################################
 
 # Função para retornar NaN se um vetor do qual se deseja extrair a moda está vazio.
 
 def conditionalMode():
   return lambda x : np.nan if x == [] else mode(x).mode[0]
 
-# Função para obtenção do agrupador KMeans cuja acurácia é máxima e cuja quantidade de clusters é mínima.
-
-def bestKM(sorted_clfs):
-  max_accuracy = sorted_clfs[-1][0]
-  return min(list(filter(lambda x : x[0] == max_accuracy, sorted_clfs)))
-
-# Função de classificação dos clusters segundo o tipo de flor mais recorrente em cada um deles
+# Function that classifies the clusters as the type of diamond more recurrent in each one of them
 
 def classifyCluster(clf, data, targets):
   predictions = clf.predict(data)
   enumerated_predictions = list(enumerate(predictions))
   elements_clusters = []
   categorized_clusters = []
-  mode_fun = conditionalMode()
-  for n in range(0, clf.n_clusters):
+  mode_fun = conditionalMode() 
+  for n in range(0, clf.n_clusters): # Assigns one class to each cluster 
     indexes_cluster = list(map(lambda x : x[0], filter(lambda x : x[1] == n, enumerated_predictions)))
     elements_clusters.append(list(map(lambda i : targets[i], indexes_cluster)))
   categorized_clusters = list(map(mode_fun, elements_clusters))
   return [categorized_clusters, enumerated_predictions]
 
-def convertPredictions(categorized_clusters, enumed_predictions):
+# Transform the predictions from clusters to classes
+
+def convertPredictions(categorized_clusters, enumed_predictions): 
   realPredictions = list(map(lambda i : categorized_clusters[i[1]], enumed_predictions))
   return realPredictions
+
+# Calculate the Score 
 
 def scoreKMeans(predictions, targets):
   got_right = list(map(lambda x : x[1] == targets[x[0]], enumerate(predictions)))
   score = sum(got_right)/len(got_right)
   return score
 
-rede = KMeans(n_clusters=69, n_init=10)
+rede = KMeans(n_clusters=30, n_init=10)
 rede.fit(attr_df)
-[catergorized_clusters, enumed_predictions] = classifyCluster(rede, attr_df, targets_df)
+[catergorized_clusters, enumed_predictions] = classifyCluster(rede, attr_test, targets_test)
 converted_predictions = convertPredictions(catergorized_clusters, enumed_predictions)
-score = scoreKMeans(converted_predictions,targets_df)
+score = scoreKMeans(converted_predictions,targets_test)
 
 print("Score = {}".format(score))
 print("Inercia = {}".format(rede.inertia_))
-print("Matriz de confusão =\n{}\n".format(confusion_matrix(converted_predictions, targets_df)))
+print("Matriz de confusão =\n{}\n".format(confusion_matrix(converted_predictions, targets_test)))
